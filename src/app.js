49915e8ec2f70ab1091d1ea3d82844b2fce1ab42
Jay Qiu <uwjjqiu@hotmail.com>
2018-05-20 19:35:40 -0400

    heroki deployment

--- src/app.js
+++ src/app.js
@@ -5,10 +5,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const path = require('path');
+const serveStatic = require('serve-static');
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
 
 const mongodb_conn_module = require('./mongodbConnModule');
 var db = mongodb_conn_module.connect();
