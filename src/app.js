d356e6c45f8a2ed63d1c74eadb49ca456cb5be42
Jay Qiu <uwjjqiu@hotmail.com>
2018-05-20 20:37:16 -0400

    update heroku

--- src/app.js
+++ src/app.js
@@ -13,11 +13,6 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
-
 const mongodb_conn_module = require('./mongodbConnModule');
 var db = mongodb_conn_module.connect();
 
